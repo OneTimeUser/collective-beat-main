97ff9a6d46133327eac6a89d321a39b619c51add
Igor Gai <igor.gai@i.ua>
2015-03-11 15:43:38 +0200

    removing django-cms-related stuff and using plain flatpages with ckeditor instead

--- apps/shows/urls.py
+++ apps/shows/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
-from apps.shows.views import ShowsList, IndexView
+from apps.shows.views import ShowsList
 
 urlpatterns = patterns('',
-    url(r'$', IndexView.as_view()),
     url(r'archive/$', ShowsList.as_view(), name='archive'),
     url(r'archive/(?P<category>\w+)/$', ShowsList.as_view(), name='archive_category'),
 )
