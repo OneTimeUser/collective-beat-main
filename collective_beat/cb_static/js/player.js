64c28779266296cc5e890e46881aee4aa9ebcff8
Stas G <kvarkson@gmail.com>
2015-03-12 16:11:42 +0200

    syncing the play/pause buttons

--- collective_beat/cb_static/js/player.js
+++ collective_beat/cb_static/js/player.js
@@ -1,42 +1,58 @@
+var playingTrackAttrs = {};
+
 $(document).ready(function() {
+    /*
+     if we need to have an initial stream or track to play
+     we put the settings here
+     */
     jwplayer("player-block").setup({
         file: "",
         height: 0,
         width: 0
     });
+
     jwplayer("player-block").onPlay(function() {
         $('.play-button img.play-button-img').hide();
         $('.play-button img.pause-button-img').show();
     });
 });
 
-function loadTrack(url, date, number, title, description, el) {
-    console.log(arguments);
+/*
+    function for starting playback and visual representation
+    for the current playing stream/playback
+ */
+function loadTrack(el) {
+    var showAttrs = playingTrackAttrs = $(el).parents('.podcast-block')[0].attributes;
+    $('.pause-button').hide();
+    $('.play-button').show();
     jwplayer("player-block").stop();
     jwplayer("player-block").remove();
     jwplayer("player-block").setup({
-        file: url,
+        file: showAttrs['data-show-url'].value,
         height: 0,
         width: 0
     }).play();
 
-    $('marquee.hidden-xs').text(description);
-    $('div.show-name').text('SILENCIO ' + number + ': ' + date + ' // ' + title);
+    $('marquee.hidden-xs').text(showAttrs['data-show-description'].value);
+    $('div.show-name').text('SILENCIO '
+        + showAttrs['data-show-number'].value
+        + ': ' + showAttrs['data-show-date'].value
+        + ' // ' + showAttrs['data-show-title'].value);
     $(el).hide();
     $(el).parent().find('.pause-button').show();
 
-    jwplayer("player-block").onPlay(function() {
-        $('.player-block .play-button').hide();
-        $('.player-block .pause-button').show();
-    });
-    jwplayer("player-block").onPause(function() {
-        $('.player-block .pause-button').hide();
-        $('.player-block .play-button').show();
-    });
+    $('.player-block .play-button').hide();
+    $('.player-block .pause-button').show();
 }
 
+/*
+    pausing button functionality
+ */
 function pause(el) {
-    $(el).hide();
-    $(el).parent().find('.play-button').show();
+    $('.pause-button').hide();
+    $('.play-button').show();
+    $('.podcast-block[data-show-id='+ playingTrackAttrs['data-show-id'].value + ']').find('.play-button').show();
+    $('.player-block .pause-button').hide();
+    $('.player-block .play-button').show();
     jwplayer("player-block").pause();
 }
