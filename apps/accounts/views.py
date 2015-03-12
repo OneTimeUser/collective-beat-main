e0e459906ddf0df66f52278f803d8928bbd052b9
Stas G <kvarkson@gmail.com>
2015-03-12 14:12:51 +0200

    pjax representation for my account

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
+from djpj import pjax_block
 
 
 class AccountInfoView(DetailView):
     template_name = 'accounts/account_info.html'
 
     @method_decorator(login_required())
+    @method_decorator(pjax_block('content'))
     def dispatch(self, request, *args, **kwargs):
         return super(AccountInfoView, self).dispatch(request, *args, **kwargs)
 
