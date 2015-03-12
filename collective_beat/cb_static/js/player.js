60797e3ea9ceb8e501cc85a3a957eaef3f80f638
Stas G <kvarkson@gmail.com>
2015-03-12 11:36:28 +0200

    playing the files from archive and update the bottom player details

--- collective_beat/cb_static/js/player.js
+++ collective_beat/cb_static/js/player.js
@@ -1,9 +1,21 @@
 $(function () {
     $(document).ready(function() {
         jwplayer("player-block").setup({
-            file: "rtmp://flash.oit.duke.edu/vod/_definst_",
+            file: "",
             height: 0,
             width: 0
         });
     });
 });
+
+function loadTrack(url, date, number, title, description) {
+    jwplayer("player-block").stop();
+    jwplayer("player-block").remove();
+    jwplayer("player-block").setup({
+        file: url,
+        height: 0,
+        width: 0
+    }).play();
+    $('marquee.hidden-xs').text(description);
+    $('div.show-name').text('SILENCIO ' + number + ': ' + date + ' // ' + title);
+}
