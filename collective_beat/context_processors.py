3697af1ec20ec6c6653c66c7a417af093d2d9393
Stas G <kvarkson@gmail.com>
2015-03-13 19:03:35 +0200

    last not live

--- collective_beat/context_processors.py
+++ collective_beat/context_processors.py
@@ -1,12 +1,12 @@
 from apps.shows.models import Show
 
 
-def live_show():
-    return Show.objects.filter(is_live=True).first()
+def last_show():
+    return Show.objects.first()
 
 
 def cb_context(request):
     context = dict(
-        live_show=live_show(),
+        last_show=last_show(),
     )
     return context
