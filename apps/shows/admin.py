4d447c8f029c26a28ee800a6ee2fee2da9ba5a4f
Igor Gai <igor.gai@i.ua>
2015-03-16 22:08:10 +0200

    ckeditor for show's description (#1760)

--- apps/shows/admin.py
+++ apps/shows/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.db import models
 from orderable.admin import OrderableAdmin
+from ckeditor.widgets import CKEditorWidget
 from apps.shows.models import ShowCategory, Show
 
 
 class ShowCategoryAdmin(OrderableAdmin):
     fields = ('title',)
 
+
+class ShowAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
 admin.site.register(ShowCategory, ShowCategoryAdmin)
-admin.site.register(Show)
+admin.site.register(Show, ShowAdmin)
